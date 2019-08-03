d2c1124221ea60dfd464f3b9aa517e2097636f6d
Krzysztof Świątek <swiatek.krzysztof95@gmail.com>
2019-08-03 15:52:06 +0200

    First commit

---
+++ src/test/java/pesel-checker/AppTest.java
@@ -0,0 +1,20 @@
+package pesel-checker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
